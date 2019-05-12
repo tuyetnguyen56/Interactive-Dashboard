003ebf2d36bff613e0732dbd72dd4fd543717d50
Tuyet <tuyet_nguyen56@hotmail.com>
2019-05-11 17:58:00 -0700

    second attempt at metadata display

--- bellybutton_app/static/js/app.js
+++ bellybutton_app/static/js/app.js
@@ -1,26 +1,51 @@
 function buildMetadata(sample) {
-
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    var url = `/metadata/${sample}`;
-    d3.json(url).then(function(sample) {
+  var url = `/meta/${sample}`;
+  d3.json(url).then(function(sample) {
 
-    // Use d3 to select the panel with id of `#sample-metadata`
-    var sample = d3.select("#sample-metadata");
+  // Use d3 to select the panel with id of `#sample-metadata`
+  var sample = d3.select("#sample-metadata");
+  
+  // Use `.html("") to clear any existing metadata
+  sample.html("");
 
-    // Use `.html("") to clear any existing metadata
-      sample.html("");
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-    Object.entries(sample).forEach(function ([key, value]) {
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
+  Object.entries(sample).forEach(function([key, value]) {
       var row = sample.append("p");
-      row.text(`${key}: ${value}`);
+      row.text(`${key}:${value}`);
   });
-  }  
-)};
+
+  });
+}
+
+
+// function buildMetadata(sample) {
+
+//   // @TODO: Complete the following function that builds the metadata panel
+
+//   // Use `d3.json` to fetch the metadata for a sample
+//     var url = `/metadata/${sample}`;
+//     d3.json(url).then(function(sample) {
+
+//     // Use d3 to select the panel with id of `#sample-metadata`
+//     var sample = d3.select("#sample-metadata");
+
+//     // Use `.html("") to clear any existing metadata
+//       sample.html("");
+
+//     // Use `Object.entries` to add each key and value pair to the panel
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
+//     Object.entries(sample).forEach(function ([key, value]) {
+//       var row = sample.append("p");
+//       row.text(`${key}: ${value}`);
+//   });
+//   }  
+// )};
 
 function buildCharts(sample) {
 
